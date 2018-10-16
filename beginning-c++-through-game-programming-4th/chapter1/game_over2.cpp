a1eee898ad22752777e3bf570482ea4d89a2d245
Neil Chen <jingsi.chen@qq.com>
2018-10-16 23:29:36 +0800

    gp4: game over 2.0
    Signed-off-by: Neil Chen <jingsi.chen@qq.com>

---
+++ beginning-c++-through-game-programming-4th/chapter1/game_over2.cpp
@@ -0,0 +1,10 @@
+// Game over 2.0
+// Demonstrates a using directive
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  cout << "Game Over!" << endl;
+  return 0;
+}
