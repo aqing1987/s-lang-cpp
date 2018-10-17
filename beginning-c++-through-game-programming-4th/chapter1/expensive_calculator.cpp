f2d62719a105775b13f048f6b04ac3a23afd81e7
Neil Chen <jingsi.chen@qq.com>
2018-10-17 22:00:43 +0800

    gp4: expensive calculator
    Signed-off-by: Neil Chen <jingsi.chen@qq.com>

---
+++ beginning-c++-through-game-programming-4th/chapter1/expensive_calculator.cpp
@@ -0,0 +1,17 @@
+// Expensive Calculator
+// Demonstrates built-in arithmetic operators
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  cout << "7 + 3 = " << 7 + 3 << endl;
+  cout << "7 - 3 = " << 7 - 3 << endl;
+  cout << "7 * 3 = " << 7 * 3 << endl;
+  cout << "7 / 3 = " << 7 / 3 << endl;
+  cout << "7.0 / 3 = " << 7.0 / 3 << endl;
+  cout << "7 % 3 = " << 7 % 3 << endl;
+  cout << "7 + 3 * 5 = " << 7 + 3*5 << endl;
+  cout << "(7+3) * 5 = " << (7+3)*5 << endl;
+  return 0;
+}
