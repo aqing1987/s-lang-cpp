984e204dd25c5e2164e12a1902db780009284149
Neil <jingsi.chen@qq.com>
2018-10-17 10:18:41 +0800

    gp4: game over 3.0
    clearly spells out which elements I plan to use. Plus, it
    doesn't make local a bunch of elements that I have no intention
    of using.
    
    Signed-off-by: Neil <jingsi.chen@qq.com>

---
+++ beginning-c++-through-game-programming-4th/chapter1/game_over3.cpp
@@ -0,0 +1,11 @@
+// Game over 2.0
+// Demonstrates a using directive
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char *argv[]) {
+  cout << "Game Over!" << endl;
+  return 0;
+}
