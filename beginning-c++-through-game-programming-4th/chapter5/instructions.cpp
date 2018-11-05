1d5ec6c770270baafe042f27e4f84f128ba7fde8
Neil <j10297116101@gmail.com>
2018-11-05 23:43:43 +0800

    gp4: instructions
    demonstrates writing new functions
    
    Signed-off-by: Neil <j10297116101@gmail.com>

---
+++ beginning-c++-through-game-programming-4th/chapter5/instructions.cpp
@@ -0,0 +1,19 @@
+// Instructions
+// Demonstrates writting new functions
+#include <iostream>
+
+using namespace std;
+
+// function prototype (declaration)
+void instructions();
+
+int main(int argc, char *argv[]) {
+  instructions();
+  return 0;
+}
+
+// function definition
+void instructions() {
+  cout << "Welcome to the most fun you've ever had with text!\n\n";
+  cout << "Here's how to play the game...\n";
+}
