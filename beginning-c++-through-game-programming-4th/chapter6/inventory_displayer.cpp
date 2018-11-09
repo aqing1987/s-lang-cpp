08bd7359c8e7ba6971d53bbfeeab70a26bb5d401
Neil <j10297116101@gmail.com>
2018-11-09 13:01:35 +0800

    gp4: inventory displayer
    Demonstrates constant reference
    
    Signed-off-by: Neil <j10297116101@gmail.com>

---
+++ beginning-c++-through-game-programming-4th/chapter6/inventory_displayer.cpp
@@ -0,0 +1,25 @@
+// Inventory Displayer
+// Demonstrates constant references
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// parameter vec is a constant reference to a vector of strings
+void display(const vector<string>& vec) {
+  cout << "Your items:\n";
+  for (vector<string>::const_iterator iter = vec.begin();
+       iter != vec.end(); ++iter) {
+    cout << *iter << endl;
+  }
+}
+
+int main(int argc, char *argv[]) {
+  vector<string> inventory;
+  inventory.push_back("sword");
+  inventory.push_back("armor");
+  inventory.push_back("shield");
+  display(inventory);
+  return 0;
+}
