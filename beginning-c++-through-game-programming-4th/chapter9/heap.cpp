2654772c92da45abf43c58d0ae77d01254363726
Neil Chen <jingsi.chen@qq.com>
2018-11-18 17:01:42 +0800

    gp4: heap
    demonstrates dynamically allocating memory
    
    Signed-off-by: Neil Chen <jingsi.chen@qq.com>

---
+++ beginning-c++-through-game-programming-4th/chapter9/heap.cpp
@@ -0,0 +1,42 @@
+// Heap
+// Demonstrates dynamically allocating memory
+
+#include <iostream>
+
+using namespace std;
+
+int* intOnHeap() {
+  int* pTemp = new int(20);
+  return pTemp;
+}
+
+void leak1() {
+  int* drip1 = new int(30);
+}
+
+void leak2() {
+  int* drip2 = new int(50);
+  drip2 = new int(100);
+  delete drip2;
+}
+
+int main(int argc, char *argv[]) {
+  int* pHeap = new int;
+  *pHeap = 10;
+  cout << "*pHeap: " << *pHeap << "\n\n";
+
+  int* pHeap2 = intOnHeap();
+  cout << "*pHeap2: " << *pHeap2 << "\n\n";
+
+  cout << "Freeing memory pointed to by pHeap.\n\n";
+  delete pHeap;
+
+  cout << "Freeing memory pointed to by pHeap2.\n\n";
+  delete pHeap2;
+
+  // get rid of dangling pointers
+  pHeap = 0;
+  pHeap2 = 0;
+  
+  return 0;
+}
